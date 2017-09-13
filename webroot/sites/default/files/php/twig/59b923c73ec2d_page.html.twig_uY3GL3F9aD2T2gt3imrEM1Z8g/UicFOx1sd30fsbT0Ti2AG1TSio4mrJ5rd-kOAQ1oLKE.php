9049e41659a00d356fe94c18db4e8d75e28eca7c
daniel <guberinadaniel@gmail.com>
2017-09-13 20:08:46 +0200

    added style and block placement change in search filter on companies page.

---
+++ webroot/sites/default/files/php/twig/59b923c73ec2d_page.html.twig_uY3GL3F9aD2T2gt3imrEM1Z8g/UicFOx1sd30fsbT0Ti2AG1TSio4mrJ5rd-kOAQ1oLKE.php
@@ -0,0 +1,276 @@
+<?php
+
+/* themes/custom/onboard_kick/templates/core/page.html.twig */
+class __TwigTemplate_93d25f57e3ca057559df2abf51b6904e868aceb55def40e050bb2d5c257dcd21 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 59);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 45
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    ";
+        // line 48
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "header", array()), "html", null, true));
+        echo "
+  </header>
+
+  ";
+        // line 51
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "primary_menu", array()), "html", null, true));
+        echo "
+  ";
+        // line 52
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "secondary_menu", array()), "html", null, true));
+        echo "
+  ";
+        // line 53
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "filter_menu", array()), "html", null, true));
+        echo "
+
+  <div class=\"layout-content-wraper\">
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 58
+        echo "
+    ";
+        // line 59
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_first", array())) {
+            // line 60
+            echo "      <aside class=\"layout-sidebar-first\" role=\"complementary\">
+        ";
+            // line 61
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "sidebar_first", array()), "html", null, true));
+            echo "
+      </aside>
+    ";
+        }
+        // line 64
+        echo "
+    <div class=\"layout-content\">
+      ";
+        // line 66
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content", array()), "html", null, true));
+        echo "
+        <div class=\"layout-content-first-second\">
+          ";
+        // line 68
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content_first", array()), "html", null, true));
+        echo "
+          ";
+        // line 69
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content_second", array()), "html", null, true));
+        echo "
+        </div>
+    </div>";
+        // line 72
+        echo "
+    ";
+        // line 73
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_second", array())) {
+            // line 74
+            echo "      <aside class=\"layout-sidebar-second\" role=\"complementary\">
+        ";
+            // line 75
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "sidebar_second", array()), "html", null, true));
+            echo "
+      </aside>
+    ";
+        }
+        // line 78
+        echo "
+  </main>
+  </div>
+  <footer role=\"contentinfo\">
+      ";
+        // line 82
+        if ($this->getAttribute(($context["page"] ?? null), "footer_service", array())) {
+            // line 83
+            echo "          ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "footer_service", array()), "html", null, true));
+            echo "
+      ";
+        }
+        // line 85
+        echo "
+      ";
+        // line 86
+        if ($this->getAttribute(($context["page"] ?? null), "footer_menu", array())) {
+            // line 87
+            echo "          ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "footer_menu", array()), "html", null, true));
+            echo "
+      ";
+        }
+        // line 89
+        echo "
+      ";
+        // line 90
+        if ($this->getAttribute(($context["page"] ?? null), "footer_copyright", array())) {
+            // line 91
+            echo "          ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "footer_copyright", array()), "html", null, true));
+            echo "
+      ";
+        }
+        // line 93
+        echo "  </footer>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/onboard_kick/templates/core/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  151 => 93,  145 => 91,  143 => 90,  140 => 89,  134 => 87,  132 => 86,  129 => 85,  123 => 83,  121 => 82,  115 => 78,  109 => 75,  106 => 74,  104 => 73,  101 => 72,  96 => 69,  92 => 68,  87 => 66,  83 => 64,  77 => 61,  74 => 60,  72 => 59,  69 => 58,  62 => 53,  58 => 52,  54 => 51,  48 => 48,  43 => 45,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{#
+/**
+ * @file
+ * Theme override to display a single page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.html.twig template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - base_path: The base URL path of the Drupal installation. Will usually be
+ *   \"/\" unless you have installed Drupal in a sub-directory.
+ * - is_front: A flag indicating if the current page is the front page.
+ * - logged_in: A flag indicating if the user is registered and signed in.
+ * - is_admin: A flag indicating if the user has permission to access
+ *   administration pages.
+ *
+ * Site identity:
+ * - front_page: The URL of the front page. Use this instead of base_path when
+ *   linking to the front page. This includes the language domain or prefix.
+ *
+ * Page content (in order of occurrence in the default page.html.twig):
+ * - node: Fully loaded node, if there is an automatically-loaded node
+ *   associated with the page and the node ID is the second argument in the
+ *   page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - page.header: Items for the header region.
+ * - page.primary_menu: Items for the primary menu region.
+ * - page.secondary_menu: Items for the secondary menu region.
+ * - page.highlighted: Items for the highlighted content region.
+ * - page.help: Dynamic help text, mostly for admin pages.
+ * - page.content: The main content of the current page.
+ * - page.sidebar_first: Items for the first sidebar.
+ * - page.sidebar_second: Items for the second sidebar.
+ * - page.footer: Items for the footer region.
+ * - page.breadcrumb: Items for the breadcrumb region.
+ *
+ * @see template_preprocess_page()
+ * @see html.html.twig
+ */
+#}
+<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    {{ page.header }}
+  </header>
+
+  {{ page.primary_menu }}
+  {{ page.secondary_menu }}
+  {{ page.filter_menu }}
+
+  <div class=\"layout-content-wraper\">
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>{# link is in html.html.twig #}
+
+    {% if page.sidebar_first %}
+      <aside class=\"layout-sidebar-first\" role=\"complementary\">
+        {{ page.sidebar_first }}
+      </aside>
+    {% endif %}
+
+    <div class=\"layout-content\">
+      {{ page.content }}
+        <div class=\"layout-content-first-second\">
+          {{ page.content_first }}
+          {{ page.content_second }}
+        </div>
+    </div>{# /.layout-content #}
+
+    {% if page.sidebar_second %}
+      <aside class=\"layout-sidebar-second\" role=\"complementary\">
+        {{ page.sidebar_second }}
+      </aside>
+    {% endif %}
+
+  </main>
+  </div>
+  <footer role=\"contentinfo\">
+      {% if page.footer_service %}
+          {{ page.footer_service }}
+      {% endif %}
+
+      {% if page.footer_menu %}
+          {{ page.footer_menu }}
+      {% endif %}
+
+      {% if page.footer_copyright %}
+          {{ page.footer_copyright }}
+      {% endif %}
+  </footer>
+</div>{# /.layout-container #}
+", "themes/custom/onboard_kick/templates/core/page.html.twig", "/var/www/html/webroot/themes/custom/onboard_kick/templates/core/page.html.twig");
+    }
+}
